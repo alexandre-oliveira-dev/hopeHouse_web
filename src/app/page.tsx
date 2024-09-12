7ebd15744dbde4f7782f882fcf28a93634fd3f14
alexandre-oliveira-dev <alexandredev.frontend@gmail.com>
2024-09-12 18:44:13 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import MainComponent from "./components/mainComponent";
 import "./page.css";
 import {Button, Col, Input, notification, Row, Space, Typography} from "antd";
 import Title from "antd/es/typography/Title";
-import animal from "./assets/animal.png";
-import leao from "./assets/leao.png";
-import elefante from "./assets/elefante.png";
+import background from "./assets/image.png";
+import background2 from "./assets/image copy.png";
+import background3 from "./assets/_AR_9469.jpg";
 import ballet from "./assets/ballet.png";
 import footbolBoys from "./assets/footbol-boys.png";
 import {MdWarning} from "react-icons/md";
@@ -32,13 +32,20 @@ export default function Home() {
             style={{
               color: "#F89825",
               margin: "0px",
-              fontSize: "65px",
+              fontSize: "70px",
               fontWeight: "800",
             }}
           >
             Hope House
           </h1>
-          <h4 style={{margin: "0px", fontWeight: "400", fontSize: "20px"}}>
+          <h4
+            style={{
+              margin: "0px",
+              fontWeight: "600",
+              fontSize: "25px",
+              color: "#fff",
+            }}
+          >
             Acreditamos que através do esporte, educação e arte vamos mudar a
             realidade de muitas crianças
           </h4>
@@ -74,7 +81,7 @@ export default function Home() {
       </section>
       <section className="section">
         <Col style={{width: "100%", height: "100%"}}>
-          <Row style={{width: "100%", height: "300px"}}>
+          <Row style={{width: "100%", height: "450px"}}>
             <div
               style={{
                 flex: 2,
@@ -85,16 +92,27 @@ export default function Home() {
                 alignItems: "center",
               }}
             >
-              <Col>
-                <Title style={{color: "#fff"}} level={1}>
+              <Col style={{width: "50%"}}>
+                <Title style={{color: "#fff", width: "65%"}} level={1}>
                   Sobre a Hope House
                 </Title>
 
-                <p style={{color: "#fff"}}>
-                  Instituto criativo, que acredita em justiça social
+                <p
+                  style={{
+                    color: "#fff",
+                    fontSize: "15px",
+                    lineHeight: "27px",
+                    fontWeight: 800,
+                  }}
+                >
+                  O Hope tem como objetivo promover a JUSTIÇA SOCIAL e
+                  TRANSFORMAÇÃO DE VIDA para os menos favorecidos através do
+                  amor, compaixão e ações, nas áreas do esporte, educação e
+                  arte.
                 </p>
                 <br />
                 <Button
+                  className="btn-style"
                   style={{
                     background: "transparent",
                     color: "#fff",
@@ -108,59 +126,229 @@ export default function Home() {
             </div>
             <div
               style={{
-                flex: 1,
-                backgroundImage: `url('${animal.src}')`,
+                flex: 1.5,
+                backgroundImage: `url('${background3.src}')`,
                 backgroundSize: "cover",
               }}
             ></div>
-            <div style={{flex: 2, backgroundColor: "#7EB7E4"}}></div>
-          </Row>
-          <Row style={{width: "100%", height: "700px", position: "relative"}}>
             <div
               style={{
                 flex: 2,
-                backgroundImage: `url('${leao.src}')`,
+                backgroundColor: "#7EB7E4",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Col style={{width: "50%"}}>
+                <Title style={{color: "#fff", width: "65%"}} level={1}>
+                  Projetos
+                </Title>
+
+                <p
+                  style={{
+                    color: "#fff",
+                    fontSize: "15px",
+                    lineHeight: "27px",
+                    fontWeight: 800,
+                  }}
+                >
+                  Hope House atua em diversas áreas como esporte, educação e
+                  arte. Além dos projetos com crianças e adolescentes,
+                  trabalhamos com as famílias dos alunos dos nossos projetos,
+                  compartilhando vivências, ensinando valores, acolhimento,
+                  auxiliando e ajudando as necessidades de cada família.
+                </p>
+                <br />
+                <Button
+                  className="btn-style"
+                  style={{
+                    background: "transparent",
+                    color: "#fff",
+                    width: "150px",
+                    height: "40px",
+                  }}
+                >
+                  Apoie-nos
+                </Button>
+              </Col>
+            </div>
+          </Row>
+          <Row style={{width: "100%", height: "auto", position: "relative"}}>
+            <div
+              style={{
+                flex: 1.14,
+                backgroundImage: `url('${background.src}')`,
                 backgroundSize: "contain",
                 backgroundAttachment: "fixed",
                 backgroundRepeat: "no-repeat",
+                display: "flex",
+                justifyContent: "flex-end",
+                alignItems: "flex-end",
               }}
             >
+              <div
+                style={{
+                  width: "400px",
+                  height: "400px",
+                  backgroundColor: "#F89825",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  padding: 10,
+                }}
+              >
+                <Col style={{width: "80%"}}>
+                  <Title style={{color: "#fff", width: "100%"}} level={1}>
+                    Últimas Noticias
+                  </Title>
+
+                  <p
+                    style={{
+                      color: "#fff",
+                      fontSize: "15px",
+                      lineHeight: "27px",
+                      fontWeight: 800,
+                    }}
+                  >
+                    Hope House atua em diversas áreas como esporte, educação e
+                    arte. Além dos projetos com crianças e adolescentes,
+                    trabalhamos com as famílias dos alunos dos nossos projetos,
+                    compartilhando vivências, ensinando valores, acolhimento,
+                    auxiliando e ajudando as necessidades de cada família.
+                  </p>
+                  <br />
+                  <Button
+                    className="btn-style"
+                    style={{
+                      background: "transparent",
+                      color: "#fff",
+                      width: "150px",
+                      height: "40px",
+                    }}
+                  >
+                    Leia agora
+                  </Button>
+                </Col>
+              </div>
+            </div>
+
+            <Col style={{flex: 2}}>
               <Row
                 style={{
-                  width: "84%",
-                  height: "50%",
-                  position: "absolute",
-                  bottom: "0px",
-                  right: "0px",
+                  backgroundColor: "#679C6C",
+                  width: "100%",
+                  alignItems: "center",
+                  padding: "2rem 2rem 2rem 4rem",
+                  display: "flex",
+                }}
+              >
+                <Col style={{flex: 1}}>
+                  <Title
+                    style={{
+                      fontSize: "70px",
+                      color: "#fff",
+                      width: "75%",
+                    }}
+                    level={1}
+                  >
+                    COMO VOCÊ PODE AJUDAR
+                  </Title>
+                  <Button
+                    className="btn-style"
+                    style={{
+                      background: "transparent",
+                      color: "#fff",
+                      width: "150px",
+                      height: "40px",
+                    }}
+                  >
+                    Saiba mais
+                  </Button>
+                </Col>
+                <div style={{flex: 1}}>
+                  <ul
+                    style={{
+                      color: "#fff",
+                      fontSize: "20px",
+                      lineHeight: "40px",
+                      fontWeight: "200",
+                    }}
+                  >
+                    <li>Apadrinhamento</li>
+                    <li>Doações</li>
+                    <li>Patrocinando Hope House</li>
+                  </ul>
+                </div>
+              </Row>
+              <Row
+                style={{
+                  width: "100%",
+                  height: "auto",
+                  flexWrap: "nowrap",
                 }}
               >
                 <div
                   style={{
-                    flex: 2,
-                    backgroundColor: "#F89825",
+                    width: "400px",
+                    height: "400px",
+                    backgroundColor: "#000",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
                   }}
-                ></div>
-                <div style={{flex: 2, backgroundColor: "#000"}}></div>
+                >
+                  <Col style={{width: "80%"}}>
+                    <Title style={{color: "#fff", width: "100%"}} level={1}>
+                      Conheça-nos
+                    </Title>
+
+                    <p
+                      style={{
+                        color: "#fff",
+                        fontSize: "15px",
+                        lineHeight: "27px",
+                        fontWeight: 800,
+                      }}
+                    >
+                      Hope House atua em diversas áreas como esporte, educação e
+                      arte. Além dos projetos com crianças e adolescentes,
+                      trabalhamos com as famílias dos alunos dos nossos
+                      projetos, compartilhando vivências, ensinando valores,
+                      acolhimento, auxiliando e ajudando as necessidades de cada
+                      família.
+                    </p>
+                    <br />
+                    <Button
+                      className="btn-style"
+                      style={{
+                        background: "transparent",
+                        color: "#fff",
+                        width: "150px",
+                        height: "40px",
+                      }}
+                    >
+                      Nossa Equipe
+                    </Button>
+                  </Col>
+                </div>
                 <div
                   style={{
-                    flex: 3,
-                    backgroundImage: `url('${elefante.src}')`,
-                    backgroundSize: "cover",
-                    backgroundRepeat: "no-repeat",
-                    backgroundPosition: "center",
+                    flex: 1,
+                    height: "400px",
+                    backgroundImage: `url('${background2.src}')`,
+                    backgroundSize: "contain",
                     backgroundAttachment: "fixed",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "right",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
                   }}
                 ></div>
               </Row>
-            </div>
-            <Col style={{flex: 3}}>
-              <Row
-                style={{
-                  backgroundColor: "#F1562A",
-                  width: "100%",
-                  height: "50%",
-                }}
-              ></Row>
             </Col>
           </Row>
         </Col>
