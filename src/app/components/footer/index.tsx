af368e91436eb84c8a2ea58f0954eb3a2066691c
alexandre-oliveira-dev <alexandredev.frontend@gmail.com>
2024-09-13 16:43:52 -0300

    update

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,7 +1,10 @@
 "use client";
 import React from "react";
 import "./style.css";
-import {Button, Col, Input} from "antd";
+import {Button, Col, Input, Row, Space, Typography} from "antd";
+import Title from "antd/es/typography/Title";
+import {FaFacebookF, FaTwitter} from "react-icons/fa";
+import {IoLogoInstagram, IoLogoYoutube} from "react-icons/io5";
 
 export default function Footer() {
   return (
@@ -12,7 +15,7 @@ export default function Footer() {
             display: "flex",
             flexDirection: "column",
             gap: 10,
-            width: "70%",
+            width: "50%",
           }}
         >
           <h2 style={{fontSize: "30px", fontWeight: "400"}}>
@@ -20,14 +23,78 @@ export default function Footer() {
           </h2>
           <Input className="input-footer-contact" placeholder="Nome"></Input>
           <Input className="input-footer-contact" placeholder="Email"></Input>
-          <Input
-            className="input-footer-contact"
-            placeholder="Telefone"
-          ></Input>
-          <Button>Enviar</Button>
+          <Space.Compact>
+            <Input
+              className="input-footer-contact"
+              placeholder="Telefone"
+            ></Input>
+            <Button
+              style={{
+                borderRadius: '0px !important',
+                outline: "none",
+                marginTop: "20px",
+                height: "40px",
+              }}
+            >
+              Enviar
+            </Button>
+          </Space.Compact>
         </Col>
       </div>
-      <div style={{flex: 1, display: "grid", placeItems: "center"}}>s</div>
+      <div style={{flex: 1, display: "grid", placeItems: "center"}}>
+        <Col>
+          <Row
+            style={{
+              justifyContent: "space-between",
+              width: "100%",
+              gap: "20px",
+            }}
+          >
+            <Col style={{display: "grid", placeItems: "center"}}>
+              <Title style={{fontWeight: 200, fontSize: "17px"}}>
+                Endereço
+              </Title>
+              <Typography.Text>endereço aqui</Typography.Text>
+            </Col>
+            <Col style={{display: "grid", placeItems: "center"}}>
+              <Title style={{fontWeight: 200, fontSize: "17px"}}>
+                Telefone
+              </Title>
+              <Typography.Text>(11) 9999-9999</Typography.Text>
+            </Col>
+            <Col style={{display: "grid", placeItems: "center"}}>
+              <Title style={{fontWeight: 200, fontSize: "17px"}}>Email</Title>
+              <Typography.Text>hopehouse@gmail.com</Typography.Text>
+            </Col>
+          </Row>
+          <br />
+          <br />
+          <Row
+            className="socialsIcons"
+            style={{justifyContent: "space-around", width: "100%", gap: "20px"}}
+          >
+            <div className="boxsocialsIcons">
+              <FaFacebookF size={25} />
+              <p style={{margin: "0px"}}>Facebook</p>
+            </div>
+            <div className="boxsocialsIcons">
+              {" "}
+              <FaTwitter size={25} />
+              <p style={{margin: "0px"}}>Twitter</p>
+            </div>
+            <div className="boxsocialsIcons">
+              {" "}
+              <IoLogoInstagram size={25} />
+              <p style={{margin: "0px"}}> Instagram</p>
+            </div>
+            <div className="boxsocialsIcons">
+              {" "}
+              <IoLogoYoutube size={25} />
+              <p style={{margin: "0px"}}>Youtube</p>
+            </div>
+          </Row>
+        </Col>
+      </div>
     </footer>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type {Metadata} from "next";
 import "./globals.css";
-import logo from ".././app/assets/logo.png";
+import logo from ".././app/assets/logoIcon.png";
 
 export const metadata: Metadata = {
   title: "Hope house",
